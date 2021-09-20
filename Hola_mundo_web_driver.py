97aa2de3576da9fd327dcc87ef26da8c5f34b98f
Luislegreaux39 <lleg231196r@gmail.com>
2021-09-20 09:31:55 -0400

    .

--- Hola_mundo_web_driver.py
+++ Hola_mundo_web_driver.py
@@ -2,9 +2,29 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome(
-    executable_path=r"./chromedriver.exe")
-MAIN_URL = "https://react-hook-form.com/faqs/"
+    executable_path=r"C:/chromedriver.exe")
+
+MAIN_URL = "https://github.com/LuisLegreaux39/Automation-Clases"
 driver.get(MAIN_URL)
+
+PAGINAS = [
+    "Code",
+    "Issues",
+    "Pull requests",
+    "Actions",
+    "Projects",
+    "Wiki",
+    "Security",
+    "Insights",
+    "Settings",
+]
+
+for pagina in PAGINAS:
+    opcion_de_barra_de_busqueda_git = driver.find_element_by_xpath(
+        "//*[@data-content='"+str(pagina)+"']")
+    opcion_de_barra_de_busqueda_git.click()
+    print("Haciendo click en ==>" + str(pagina))
+    time.sleep(5)
+
 time.sleep(10)
-print("Accessing to a page")
 driver.close()
---
+++ Repaso.py
@@ -0,0 +1,23 @@
+# print("Mostrando mensaje")
+
+
+# variable = 'Mensaje de texto'
+# variable_con_Numeros = 23.232
+
+# print(variable)
+# print(variable_con_Numeros)
+
+
+# def funcion1():
+#     print("Estoy adentro de la funcion")
+
+
+# funcion1()
+
+
+# Bucle
+# Una sentencia de codigo que se repite
+# For , While y Do..While
+
+for i in range(0, 10):
+    print(str(i) + "-Hola")
